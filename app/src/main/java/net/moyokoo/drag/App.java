e66c6c76f91278d9b1dd0576c94fad19baa0650d
guoyong <guoyong@emcc.net.com>
2018-11-28 16:34:23 +0800

    Diooto

---
+++ app/src/main/java/net/moyokoo/drag/App.java
@@ -0,0 +1,10 @@
+package net.moyokoo.drag;
+
+import android.app.Application;
+
+
+public class App extends Application {
+    @Override public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ diooto/build/generated/source/buildConfig/debug/net/moyokoo/drag/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package net.moyokoo.drag;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "net.moyokoo.drag";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 15;
+  public static final String VERSION_NAME = "1.5";
+}
